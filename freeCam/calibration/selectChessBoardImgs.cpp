b66f9493c9e939631bf2b82ba68fcca890b43011
BrownOfSummer <l846093185@163.com>
2018-07-03 14:02:02 +0800

    changes for show

--- freeCam/calibration/selectChessBoardImgs.cpp
+++ freeCam/calibration/selectChessBoardImgs.cpp
@@ -17,6 +17,7 @@
 #define IN_CORNER_WIDTH 7
 #define IN_CORNER_HEIGHT 5
 #define SKIP_FRAME_NUM 10
+#define MAX_WIDTH_FOR_SHOW 640
 using namespace std;
 using namespace cv;
 vector<string> listDir( const char* path, const char *ext );
@@ -25,12 +26,12 @@ void help()
     printf("Demonstrate the process of chessboard image selection.\n");
     printf("Input a dir contain a list of images or a video.\n");
     printf("ESC to break, s for select, other for next.\n");
-    printf("Usage:\n\t ./selectChessBoardImgs -I ../path/to/imageDir/\n\t ./selectChessBoardImgs -V ../path/to/video.mp4 ../dir/to_save_images/\n");
+    printf("Usage:\n\t ./selectChessBoardImgs -I ../path/to/imageDir/ /path/to/selected.txt\n\t ./selectChessBoardImgs -V ../path/to/video.mp4 ../dir/to_save_images/\n");
     return;
 }
 int main(int argc, char *argv[])
 {
-    if(argc != 3 && argc != 4)
+    if(argc != 4)
     {
         printf("ERROR INPUT\n");
         help();
@@ -41,21 +42,11 @@ int main(int argc, char *argv[])
     {
         printf("Video mode !\n");
         isVideo = true;
-        if(argc != 4) {
-            help();
-            printf("ERROR INPUT\n");
-            return -1;
-        }
     }
     else if( !strcmp(argv[1], "-I") )
     {
         printf("Image list mode.\n");
         isVideo = false;
-        if(argc != 3) {
-            help();
-            printf("ERROR INPUT\n");
-            return -1;
-        }
     }
     else
     {
@@ -74,9 +65,9 @@ int main(int argc, char *argv[])
         printf("Totall get %d image file.\n", static_cast<int>(imageNames.size()));
         
         vector<string> selectedFilePath;
-        for(size_t i = 0; i < imageNames.size(); ++i)
+        for(size_t idx = 0; idx < imageNames.size();)
         {
-            string imagePath = string( imageDir ) + imageNames[i]; // imageDir like ../path/
+            string imagePath = string( imageDir ) + imageNames[idx]; // imageDir like ../path/
             Mat view = imread(imagePath, 1);
             if( view.empty() ){
                 printf("Load image %s Failed.\n", imagePath.c_str());
@@ -92,7 +83,17 @@ int main(int argc, char *argv[])
                 cvtColor(view, viewGray, COLOR_BGR2GRAY);
                 cornerSubPix( viewGray, pointBuf, Size(11,11), Size(-1,-1), TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 30, 0.1 ));
                 drawChessboardCorners( view, boardSize, Mat(pointBuf), found );
-                imshow("s-select,ESC-break,other-next", view);
+            }
+
+            if(view.cols > MAX_WIDTH_FOR_SHOW) {
+                double ratio = MAX_WIDTH_FOR_SHOW / (double)(view.cols);
+                resize(view, view, Size(), ratio, ratio);
+            }
+            
+            /*If found, show and save*/
+            if( found )
+            {
+                imshow("s-select,ESC-break,p->prev-image,other-next", view);
                 char k = waitKey();
                 if(k == 's') {
                     char *imageAbsPath = realpath(imagePath.c_str(), NULL);
@@ -100,15 +101,24 @@ int main(int argc, char *argv[])
                     selectedFilePath.push_back(imageAbsPath);
                 }
                 else if(k == 27) break;
+                else if(k == 'p') idx = idx - 1 > 0 ? idx - 1 : 0;
+                else idx ++;
             }
-            else printf("No found in %s\n", imageNames[i].c_str());
+            else
+            {
+                idx ++;
+                printf("No found in %s\n", imageNames[idx].c_str());
+                imshow("s-select,ESC-break,p->prev-image,other-next", view);
+                waitKey(30);
+                continue;
+            }
         }
 
         /*save abspath to selected.txt*/
         if( selectedFilePath.size() > 0 )
         {
-            printf("Save the path to selected.txt\n");
-            FILE *fp = fopen("selected.txt","w");
+            printf("Save the paths to %s\n", argv[3]);
+            FILE *fp = fopen(argv[3],"w");
             if( fp == NULL ) {
                 printf("Open selected.txt failed.\n");
                 return -1;
@@ -151,11 +161,24 @@ int main(int argc, char *argv[])
                 cvtColor(view, gray, COLOR_BGR2GRAY);
                 cornerSubPix( gray, pointBuf, Size(11,11), Size(-1,-1), TermCriteria( TermCriteria::EPS+TermCriteria::COUNT, 30, 0.1 ));
                 drawChessboardCorners( view, boardSize, Mat(pointBuf), found );
+            }
+            if(view.cols > MAX_WIDTH_FOR_SHOW)
+            {
+                double ratio = MAX_WIDTH_FOR_SHOW / (double)(view.cols);
+                resize(view, view, Size(), ratio, ratio);
+            }
+            if( found )
+            {
                 imshow("s-select,ESC-break,other-next", view);
                 char k = waitKey();
                 if(k == 's') selectedImages.push_back(frame.clone());
                 else if(k == 27) break;
                 else continue;
+            }
+            else
+            {
+                imshow("s-select,ESC-break,other-next", view);
+                waitKey(30);
             }
         }
         /*save the select image to image path*/
@@ -164,11 +187,11 @@ int main(int argc, char *argv[])
         {
             for(size_t i = 0; i < selectedImages.size(); i ++) {
                 sprintf(save_path,"%sselected_%04d.jpg",argv[3], (int)(i));
-                imshow("selected", selectedImages[i]);
+                //imshow("selected", selectedImages[i]);
                 printf("save to %s\n", save_path);
                 imwrite(save_path, selectedImages[i]);
-                char k = waitKey();
-                if( k == 27 ) break;
+                //char k = waitKey();
+                //if( k == 27 ) break;
             }
         }
     }
